4b0f96e71f2ffdde7703e5fbb23224ff1135b9cb
AdrianCo97 <adrian.corazza@outlook.com>
2021-10-12 12:03:05 +0200

    Created some enums

--- project/app.js
+++ project/app.js
@@ -1,14 +1,40 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+/* const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Adrian",
+  age: 24,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+}; */
+/* const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2; */
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READONLY"] = 1] = "READONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: "Adrian",
+    age: 24,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
+};
+//person.role.push("admin");
+//person.role[1] = 10;
+//person.role = [0, 'admin', 'user']
+var favouriteActivities;
+favouriteActivities = ["Sports"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
